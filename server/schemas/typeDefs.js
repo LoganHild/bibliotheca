65c18ef2550662f6127958fcb80a3d8345439edb
Logan Hildebrandt <hildebrandtlogan@gmail.com>
2021-11-22 12:58:17 -0600

    type auth is throwing an error, unsure how to do the typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -24,12 +24,6 @@ const typeDefs = gql`
     type Query {
         me: User
     }
-    type Mutation {
-        login(email: String!, password: String!): auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(bookData: BookInput!): User
-        removeBook(bookId: ID!): User
-    }
 `;
 
 module.exports = typeDefs;
