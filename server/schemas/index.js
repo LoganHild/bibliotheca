3af422724f5e20f58c561ddf3a7e69b7a11687a4
Logan Hildebrandt <hildebrandtlogan@gmail.com>
2021-11-22 12:32:05 -0600

    added code to the index.js file to use the typeDefs and resolvers

---
+++ server/schemas/index.js
@@ -0,0 +1,6 @@
+//requires the files to be used
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+//exports the files to be used elsewhere
+module.exports = { typeDefs, resolvers }
